56a0d0dbceab7a2819dc1dd42b180a1c97e9bf1f
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-04-20 20:50:50 +0530

    Added Curl command

---
+++ .github/curl.sh
@@ -0,0 +1,18 @@
+# this is when running from production
+curl -X POST https://newletter-service.vercel.app/api/v1/email/send \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: [ENCRYPTION_KEY]" \
+  -d '{
+    "emails": "harshita.joshi2024@nst.rishihood.edu.in shreya.suman2024@nst.rishihood.edu.in rachit.gupta2024@nst.rishihood.edu.in anurag.tiwari2024@nst.rishihood.edu.in yashika.b25572@nst.rishihood.edu.in vaidehi.sahu2024@nst.rishihood.edu.in",
+    "subject": "The AIRIS Chronicle – Latest Edition"
+  }'
+
+
+# This is when running locally 
+curl -X POST http://localhost:5001/api/v1/email/send \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: [ENCRYPTION_KEY]" \
+  -d '{
+    "emails": "harshita.joshi2024@nst.rishihood.edu.in shreya.suman2024@nst.rishihood.edu.in rachit.gupta2024@nst.rishihood.edu.in anurag.tiwari2024@nst.rishihood.edu.in yashika.b25572@nst.rishihood.edu.in vaidehi.sahu2024@nst.rishihood.edu.in",
+    "subject": "The AIRIS Chronicle – Latest Edition"
+  }'
