d159eadec42b15fe3f4b68b5df931783a9b139ce
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2026-04-21 09:59:48 +0530

    updated scripts

--- .github/curl.sh
+++ .github/curl.sh
@@ -16,3 +16,12 @@ curl -X POST http://localhost:5001/api/v1/email/send \
     "emails": "harshita.joshi2024@nst.rishihood.edu.in shreya.suman2024@nst.rishihood.edu.in rachit.gupta2024@nst.rishihood.edu.in anurag.tiwari2024@nst.rishihood.edu.in yashika.b25572@nst.rishihood.edu.in vaidehi.sahu2024@nst.rishihood.edu.in",
     "subject": "The AIRIS Chronicle – Latest Edition"
   }'
+
+# from render production 
+curl -X POST https://newletter-service.onrender.com/api/v1/email/send \
+  -H "Content-Type: application/json" \
+  -H "x-api-key: [ENCRYPTION_KEY]" \
+  -d '{
+    "emails": "anurag.tiwari2024@nst.rishihood.edu.in",
+    "subject": "The AIRIS Chronicle – Latest Edition"
+  }'
